8b198c4e46fde9dc8c9543ee0855397e84f650fa
John Cato <johncato@Johns-MBP.home>
2019-10-23 23:57:53 -0400

    Sets up RSpec-rails, creates test db, and configures rspec.

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module BlogSite
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_spec: false,
+        routing_specs: false
+    end
   end
 end
