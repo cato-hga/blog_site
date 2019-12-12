2fd1780ca59d24f25ac3dde16405cfa2efc27fc3
John Cato <jchristopher.cato@gmail.com>
2019-12-11 21:23:51 -0500

    Adds update actions and controller tests.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     if @post.save
       flash[:notice] = 'Post saved successfully!'
-      redirect_to(@post)
+      redirect_to @post
     else
       flash.now[:alert] = 'Post was unable to save!'
       render :new
@@ -20,6 +20,15 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update post_params
+      redirect_to @post
+    else
+      render :edit
+    end
   end
 
   private
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -18,9 +18,30 @@ RSpec.describe PostsController, type: :controller do
 
   describe "GET #show" do
     let(:post) { create :post }
+
     it "returns http success" do
       get :show, params: { id: post.id }
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST #create" do
+    let(:post) { create :post }
+
+    it "should create a new post" do
+      expect { create(:post) }.to change { Post.count }.by(1)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "PATCH #update" do
+    let(:post) { create :post }
+
+    it "updates a post" do 
+      post_params = FactoryBot.attributes_for(:post, title: "Update Post Title")
+
+      patch :update, params: { id: post.id, post: post_params }
+      expect(post.reload.title).to eq "Update Post Title"
     end
   end
 end
