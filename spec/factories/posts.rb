65117dd4b183ce61cf33ffee37ffac3cd8ddf85e
John Cato <jchristopher.cato@gmail.com>
2020-03-29 20:01:19 -0400

    Fixed Active Storage issue

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  body        :text
+#  date        :date
+#  slug        :string
+#  image       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :integer
+#  user_id     :integer
+#
+
 FactoryBot.define do
   factory :post do
     title "New Post"
