bcf107358aab74e5798ccd118565e505a1666ef4
John Cato <jchristopher.cato@gmail.com>
2020-05-05 15:13:54 -0400

    Set white-space to nowrap, add google fonts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,8 @@
 
 Post.destroy_all
 
-posts = Post.create!([{title: "hksdkhsdkhshkdssdksdkskdskskhdksdskhkshdkshkshdkshdkshdkshdksh", body: "khaskahasahkjsah", created_at: Date.today},{title: "TESTljsjlsls", body: "khaskahasahkjsah", created_at: 1.year.ago},{title: "TESTljsjlsls", body: "khaskahasahkjsah", created_at: 1.year.ago},{title: "TESTljsjlsls", body: "khaskahasahkjsah", created_at: 1.year.ago},{title: "TESTljsjlsls", body: "khaskahasahkjsah", created_at: 1.year.ago},{title: "TESTljsjlsls", body: "khaskahasahkjsah", created_at: 1.year.ago},{title: "TESTljsjlsls", body: "khaskahasahkjsah", created_at: 1.year.ago},{title: "TESTljsjlsls", body: "khaskahasahkjsah", created_at: 1.year.ago},{title: "TESTljsjlsls", body: "khaskahasahkjsah", created_at: 1.year.ago},{title: "TESTljsjlsls", body: "khaskahasahkjsah", created_at: 1.year.ago},{title: "TESTljsjlsls", body: "khaskahasahkjsah", created_at: 1.year.ago},{title: "TESTljsjlsls", body: "khaskahasahkjsah", created_at: Date.today}])
+posts = Post.create!([{title: Faker::Name.name, body: "khaskahasahkjsah", created_at: 1.year.ago},
+                      {title: "Learning Spanish", body: "khaskahasahkjsah", created_at: 1.year.ago},
+                      {title: Faker::Name.name, body: "khaskahasahkjsah", created_at: 1.year.ago},
+                      {title: "TESTljsjlsls", body: "In the last decade, while phone cameras have gone from shooting tiny noisy pictures to producing DSLR-level quality shots, webcams have coasted on the mediocre lane. As distributed or remote work became more common, we’ve never relied on webcams more yet they’re just as bad as they were ten years ago. The Logitech C920 (~$50), still The Wirecutter’s top pick in 2020, came out in January 2012 — the year of the chamfered-edged iPhone 5.", created_at: Date.today}
+                    ])
